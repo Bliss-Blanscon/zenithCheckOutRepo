570bbaa932adfc02d0974f8adc962022ef05d3fc
Asura <kiba741@gmail.com>
2025-06-05 13:28:43 +0000

    Add navigation links to hero section

--- app/components/home/heroSection.tsx
+++ app/components/home/heroSection.tsx
@@ -5,6 +5,13 @@ const heroSection = () => {
     <div>
       <section className="bg-[url(https://plus.unsplash.com/premium_photo-1694141250007-fc4711bb9df1?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-center h-screen w-screen">
         <div className="h-screen w-screen bg-black/25 flex flex-col text-center justify-center items-center gap-4">
+          <div className="flex gap-4 text-white absolute top-4 left-4 font-bold text-xs">
+            <a href="">ABOUT</a>
+            <a href="">MENU</a>
+            <a href="">SERVICES</a>
+            <a href="">CONTACT</a>
+          </div>
+
           <h1 className="text-9xl font-bold font-[croissant-one] text-white">
             Nududu
           </h1>
